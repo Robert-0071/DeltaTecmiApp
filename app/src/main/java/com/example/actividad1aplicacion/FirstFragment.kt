54b45c441321102bca1178e4f35485a93a4fe9aa
Robert-0071 <deleonroberto292@gmail.com>
2026-08-18 21:18:30 -0600

    Cleaned up template comments and finalized UI logic

--- app/src/main/java/com/example/actividad1aplicacion/FirstFragment.kt
+++ app/src/main/java/com/example/actividad1aplicacion/FirstFragment.kt
@@ -10,15 +10,9 @@ import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.actividad1aplicacion.databinding.FragmentFirstBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
